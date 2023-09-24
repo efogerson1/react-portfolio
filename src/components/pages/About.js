5be5b522094e50c583b7c8292e47a4dd75d4038d
efogerson1 <efogerson2@gmail.com>
2023-09-24 14:50:10 -0700

    added bio

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,13 +6,11 @@ const About = () => (
     <h1 class="pro-title">About Me</h1>
     <img className="eric" src={ericprofilepic} alt="photo of Eric hiking" />
     <p className="para">
-  I live in the Pacific Northwest, and am an Fullstack Programmer.
+    Welcome to my online portfolio! I'm a full-stack developer based in the beautiful Pacific Northwest, specifically the Portland area. My portfolio is a reflection of my passion for coding and my dedication to creating exceptional web experiences. I have experience with a diverse range of programming languages and frameworks, including HTML, CSS, responsive design, Bootstrap, React, JavaScript, Node.js, Express.js, MySQL, MongoDB, and Progressive Web Apps. Each project in my portfolio showcases my skills and creativity, highlighting my ability to bring ideas to life through clean and efficient code. Explore my work, and feel free to reach out if you'd like to collaborate or discuss potential opportunities.
     </p>
     <br />
     <br />
-    <p className="para">
-    More stuff here
-    </p>
+   
 
   </div>
 );
